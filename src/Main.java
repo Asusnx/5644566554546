fbcc2be669019dc7cb281b660945ad52a4eb447b
Asusnx <asus0111@gmail.com>
2024-03-19 14:39:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 19;
+        suma (a, b);
+    }
+    public static void suma (int x, int y){
+        int suma2 = x + y;
+        System.out.println("suma " + suma2);
+
+    }
+    {System.out.println("2uzduotis");
+    }
+    
+
+}
+
